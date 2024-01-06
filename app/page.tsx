4b84ea382883e0d7f9f4e6bb5d40b075d2975f2b
nichu-srini331 <nichubg3.1@gmail.com>
2024-01-07 01:16:39 +0530

    Final Commit

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,7 @@ import HeadsetIcon from '@mui/icons-material/Headset';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
   const [click,isclick] = useState(false)
@@ -37,10 +38,14 @@ export default function Home() {
   }
   return (
     <main className='container-fluid m-0 p-0' >
-  <section className='sm:p-2 flex justify-center items-center'>
-    <h2 className='px-5 text-center text-lg md:text-base lg:text-xl'>Announcement</h2>
-    <p className='text-center text-sm md:text-base lg:text-lg sm:p-0 sm:text-base'>We are happy to announce that we raised $2 Million in Seed Funding</p>
-  </section>
+  <section className='sm:p-2 sm:flex justify-center items-center'>
+  <Marquee>
+  <h2 className='pr-3 text-center text-lg sm:text-base md:text-xl lg:text-xl xl:text-2xl'>
+  Announcement : 
+</h2>
+    <p className=' text-center text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl'>
+  We are happy to announce that we raised $2 Million in Seed Funding
+</p></Marquee>  </section>
   <section className='container-fluid flex justify-between sm:pr-10 s2'>
     <nav className="w-full">
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
@@ -59,37 +64,44 @@ export default function Home() {
                 </div>
                 <div className=' hidden md:block ml-40'>
                   <div className='ml-5 flex justify-between item-center space-x-4'>
-                    <a href='/' className='text-black pl-11 mt-4'>Card access</a>
+                    <a href='/' className='text-black pl-11 mt-4'>Card</a>
                     <a href='/' className='text-black pl-6 mt-4'>Banking</a>
                     <a href='/' className='text-black pl-5 mt-4'>Processing</a>
                     <a href='/' className='text-black pl-9 mt-4'>About</a>
                     <a href='/' className='text-black pl-9 mt-4'>Career</a>
                     <a href='/' className='text-black pl-9 mt-4'>Contact</a>
                   </div>
+                  
                 </div>
+                <Button variant="contained" className=" bg-purple-800 h-7 mb-4  sm:w-32 sm:h-10 md:w-40 md:h-20 lg:w-28 lg:h-12 mt-2  flex justify-center items-center" endIcon={<ArrowForwardIcon/>}>
+    LogIn
+  </Button>
                 <div className='md:hidden flex items-center '>
-                  <button className=' inline-flex item-center justify-end p-2 ' onClick={toggleNav}>{click ?(<svg className='h-6 w-6' xmlns="https://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round'strokeWidth={2} d="M6 18L18 6M6 6l12 12"/></svg>):(<svg className='h-6 w-6' xmlns="https://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round'strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7"/></svg>)}</button>
+                  <button className='inline-flex  justify-end' onClick={toggleNav}>{click ?(<svg className='h-6 w-6' xmlns="https://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round'strokeWidth={2} d="M6 18L18 6M6 6l12 12"/></svg>):(<svg className='h-6 w-6' xmlns="https://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke='currentColor'><path strokeLinecap='round' strokeLinejoin='round'strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7"/></svg>)}</button>
+                 
                 </div>
               </div>
             </div>
             {click && (
               <div className='md:hidden'>
-                <div className='flex ml-7 px-2 pt-2 pb-3 space-y-1'>
-                <a href='/' className='text-black block'>Card access</a>
-                    <a href='/' className='text-black block'>Banking</a>
-                    <a href='/' className='text-black block'>Processing</a>
-                    <a href='/' className='text-black block'>About</a>
-                    <a href='/' className='text-black block'>Career</a>
-                    <a href='/' className='text-black block'>Contact</a>
-                </div>
+              <div className='ml-7 px-2 pt-2 pb-3 space-y-1'>
+                <a href='/' className='block text-black'>Card</a>
+                <a href='/' className='block text-black'>Banking</a>
+                <a href='/' className='block text-black'>Processing</a>
+                <a href='/' className='block text-black'>About</a>
+                <a href='/' className='block text-black'>Career</a>
+                <a href='/' className='block text-black'>Contact</a>
               </div>
+            </div>
+            
             )}
           </nav>
           {/* <div className="shadow-lg ml-20 flex">Login <IoIosLogIn /></div> */}
-          <div className='w-50 h-70 mt-2 md:mt-5 ml-[25%] justify-center items-center'><Button variant="contained" className="bg-purple-800" endIcon={<ArrowForwardIcon/>}>
-  Log 
-</Button></div>
-        </section>
+          
+  
+
+
+        </section>  
         <section>
   <div className='flex justify-center items-center pt-10'>
     <div className='m1-d relative w-[631px] h-[460px] item-center'>
@@ -100,48 +112,58 @@ export default function Home() {
         </div>
       </div>
             <div><div className='font-inter text-base font-normal leading-[28px] text-center text-gray-700 mt-5'>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</div></div>
-            <div className=' mr-4 mt-5 md:mt-5 ml-[35%] justify-center items-center'><Button variant="contained" className="bg-purple-800" endIcon={<ArrowForwardIcon/>}>
+            <div className=' mt-5 md:mt-5 flex justify-center items-center'><Button variant="contained" className="bg-purple-800" endIcon={<ArrowForwardIcon/>}>
   Unlock your Card
 </Button></div>
-<div><div className='mt-3  md:font-inter text-base font-normal leading-[28px] text-center text-gray-700 '>*No credit card required</div></div>
+<div><div className='mt-3 md:font-inter text-base font-normal leading-[28px] text-center text-gray-700 '>*No credit card required</div></div>
 </div>
           </div>
         </section>
         <section className='mt-9'>
       <div className='flex flex-wrap justify-between px-3 md:px-6 lg:px-8 xl:px-12 mt-3 sm:my-4'>
         <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2 flex justify-center'>
-          <Image src={Nuxt} width={104} height={40} alt=""/>
+          <Image src={Nuxt} width={104} height={40} alt="" className='m-3'/>
         </div>
         <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2 flex justify-center'>
-          <Image src={Mir} width={114} height={40} alt=""/>
+          <Image src={Mir} width={114} height={40} alt="" className='m-3'/>
         </div>
         <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2 flex justify-center'>
-          <Image src={Lottie} width={114} height={40} alt=""/>
+          <Image src={Lottie} width={114} height={40} alt="" className='m-3'/>
         </div>
         <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2 flex justify-center'>
-          <Image src={wise} width={114} height={40} alt=""/>
+          <Image src={wise} width={114} height={40} alt="" className='m-3'/>
         </div>
         <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2 flex justify-center'>
-          <Image src={dribble} width={114} height={40} alt=""/>
+          <Image src={dribble} width={114} height={40} alt="" className='m-3'/>
         </div>
         <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2 flex justify-center'>
-          <Image src={kinsta} width={114} height={40} alt=""/>
+          <Image src={kinsta} width={114} height={40} alt="" className='m-3'/>
         </div>
         <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2 flex justify-center'>
-          <Image src={angel} width={114} height={40} alt=""/>
+          <Image src={angel} width={114} height={40} alt="" className='m-3'/>
         </div>
         <div className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2 flex justify-center'>
-          <Image src={behance} width={114} height={40} alt=""/>
+          <Image src={behance} width={114} height={40} alt="" className='m-3'/>
         </div>
        
       </div>
     </section>
         <section  className='flex justify-center mt-7'>
-  <div className=' flex  items-center pt-10'>
-    <div className=''>
-      <div className=' flex justify-center font-manrope text-2xl font-semibold leading-[48px] text-center'>Elevating Card Programs with Cutting-Edge Technology</div>
-      <div className='font-inter  font-normal leading-[38px] text-gray-700  h-[66px] mt-5 w-64 md:w-auto truncate text-sm'>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-      <div className='flex justify-center mt-6  text-purple-800'><a><div>Compare all Pro features<ArrowForwardIcon/></div></a></div>
+  <div className='   items-center pt-10'>
+    <div className='flex  flex-col'>
+      <div className='flex justify-center font-manrope text-2xl font-semibold leading-[48px] text-center'>Elevating Card Programs with Cutting-Edge Technology</div>
+      <div className='mt-5 md:mt-5'>
+  <div className='font-inter font-normal leading-[38px] text-gray-700 h-[66px] mt-5 w-full md:w-auto text-sm text-center md:text-left'>
+    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </div>
+
+  <div className='flex justify-center md:justify-center md:justify-start mt-6 text-purple-800'>
+    <a href='' className=''>
+      Compare all Pro features<ArrowForwardIcon />
+    </a>
+  </div>
+</div>
+
     </div>
   </div>
 </section>
@@ -169,9 +191,9 @@ export default function Home() {
           
         </section>
         <section>
-          <div className='flex flex-wrap justify-center items-center'>
-            <Image src={pht} width={500} height={400} alt='' className='mr-10'/>
-            <Image src={stat} width={500} height={400} alt=''/>
+          <div className='flex flex-wrap  justify-center items-center'>
+            <Image src={pht} width={500} height={400} alt='' className='m-5'/>
+            <Image src={stat} width={500} height={400} alt='' className='m-5'/>
           </div>
         </section>
         <section>
@@ -201,7 +223,9 @@ export default function Home() {
         <section className='flex flex-wrap justify-center '>
           <div>
             <div className='flex justify-center p-11'><Image src={Vect} width={100} height={25} alt=""/></div>
-            <div className='font-manrope text-2xl font-medium leading-[40px] text-center justify-center ml-10'>I had the pleasure of experiencing the next generation of card solutions with this incredible product. Its refreshing to see such innovation in the financial industry.</div>
+            <div className='font-manrope  text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-2xl font-medium leading-[40px] text-center justify-center ml-10'>
+  I had the pleasure of experiencing the next generation of card solutions with this incredible product. It's refreshing to see such innovation in the financial industry.
+</div>
             <div className='flex justify-center p-11'><Image src={profile} width={100} height={30} alt=""/></div>
             <div className='txt-6'>Nick Babich</div>
             <div className='txt-61'>Lead Designer</div>
@@ -224,7 +248,7 @@ export default function Home() {
       alt="Picture of the author"
       className='flex justify-center'
     /></div>
-    <div className='text-gray-700 font-inter text-base font-medium leading-[28px] text-center w-102 h-82 whitespace-normal md:whitespace-nowrap'>Mode UI is a comprehensive design system that empowers designers and developers to create cohesive and visually stunning user interfaces across various platforms and devices</div>
+    <div className='text-gray-700 font-inter text-base font-medium leading-[28px] text-center md:w-62 h-82 '>Mode UI is a comprehensive design system that empowers designers and developers to create cohesive and visually stunning user interfaces across various platforms and devices</div>
           </div>
         </section>
         <section>
