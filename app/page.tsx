3d92127269d5b6c2b4f28483c94b6b1379d949c6
nichu-srini331 <nichubg3.1@gmail.com>
2024-01-07 01:28:58 +0530

    ap commit

--- app/page.tsx
+++ app/page.tsx
@@ -244,7 +244,7 @@ export default function Home() {
           <div>
             <div className='flex justify-center p-11'><Image src={Vect} width={100} height={25} alt=""/></div>
             <div className='font-manrope  text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-2xl font-medium leading-[40px] text-center justify-center ml-10'>
-  I had the pleasure of experiencing the next generation of card solutions with this incredible product. It's refreshing to see such innovation in the financial industry.
+  I had the pleasure of experiencing the next generation of card solutions with this incredible product. Its refreshing to see such innovation in the financial industry.
 </div>
             <div className='flex justify-center p-11'><Image src={profile} width={100} height={30} alt=""/></div>
             <div className='txt-6'>Nick Babich</div>
